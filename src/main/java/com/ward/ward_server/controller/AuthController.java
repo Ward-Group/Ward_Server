8737b5440257cbadcc4d87458220e912a585a6b1
s0nnyday <lijnar100@naver.com>
2023-12-07 03:51:25 +0900

    fix : 필요없는 코드 제거
    필요없는 코드 제거

--- src/main/java/com/ward/ward_server/controller/AuthController.java
+++ src/main/java/com/ward/ward_server/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.ward.ward_server.model.LoginResponse;
 import com.ward.ward_server.security.JwtIssuer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,11 @@ import java.util.List;
 public class AuthController {
     private final JwtIssuer jwtIssuer;
 
+    @GetMapping("/")
+    public String greeting(){
+        return "Hello, World";
+    }
+    
     @PostMapping("/auth/login")
     public LoginResponse login(@RequestBody @Validated LoginRequest request){
         var token = jwtIssuer.issue(1L, request.getEmail(), List.of("USER"));
