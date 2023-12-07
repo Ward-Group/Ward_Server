f89fe593928596c941c72fda22d19a3100ea2241
s0nnyday <lijnar100@naver.com>
2023-12-07 22:39:59 +0900

    Chore : 사용하지 않는 import 제거
    사용하지 않는 GetMapping import 문 삭제

--- src/main/java/com/ward/ward_server/controller/AuthController.java
+++ src/main/java/com/ward/ward_server/controller/AuthController.java
@@ -5,7 +5,6 @@ import com.ward.ward_server.model.LoginResponse;
 import com.ward.ward_server.security.JwtIssuer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
