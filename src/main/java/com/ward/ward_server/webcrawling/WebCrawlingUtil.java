dd555092e362b5f6b90f5ebbd440aca5b41271ee
mewluee <yunide073@gmail.com>
2023-12-16 00:11:07 +0900

    나이키 SNKRS 런치 캘린더 웹 크롤러 구현

---
+++ src/main/java/com/ward/ward_server/webcrawling/WebCrawlingUtil.java
@@ -0,0 +1,87 @@
+package com.ward.ward_server.webcrawling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Slf4j
+public class WebCrawlingUtil {
+    //FIXME 나중에 사이트별로 웹 크롤러 만들때 다 쓰이니까 전역변수로 만들어줘야 한다.
+    private String CHROME_DRIVER_PATH = "D:\\chromedriver/chromedriver-win64/chromedriver.exe";
+
+    private WebDriver driver;
+
+    @Autowired
+    public WebCrawlingUtil() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.addArguments("--headless=new"); //헤드리스 모드로 실행, 실제 창이 표시되지 않는다.
+        chromeOptions.addArguments("--lang=ko"); //브라우저 언어를 한국어로 설정
+        chromeOptions.addArguments("--no-sandbox"); //샌드박스 모드 비활성화
+        chromeOptions.addArguments("--disable-dev-shm-usage"); // /dev/shm 사용 비활성화
+        chromeOptions.addArguments("--disable-gpu"); //GPU 가속 비활성화
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
+        this.driver = new ChromeDriver(chromeOptions);
+    }
+
+    public List<String> getPage() {
+        String url = "https://www.nike.com/kr/launch?s=upcoming"; //나이키(Nike) SNKRS 런치 캘린더 KR
+        driver.get(url);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        List<String> results = new ArrayList<>();
+        List<WebElement> elements = driver.findElements(By.cssSelector(".product-card.ncss-row.mr0-sm.ml0-sm"));
+        for (WebElement parent : elements) {
+            String month = parent.findElement(By.cssSelector(".launch-caption .headline-4")).getText();
+            String day = parent.findElement(By.cssSelector(".launch-caption .headline-1")).getText();
+            String productName = parent.findElement(By.cssSelector(".figcaption-content .headline-5")).getText();
+            WebElement imgElement = parent.findElement(By.cssSelector("div.ncss-col-sm-12 a.card-link img.image-component"));
+            String imgUrl = imgElement.getAttribute("src");
+            results.add(month + " " + day + "," + productName + ", url=" + imgUrl);
+        }
+        return results;
+    }
+
+    public List<WebProductData> getPageWithJSoup() {
+        String url = "https://www.nike.com/kr/launch?s=upcoming";
+        driver.get(url);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        String html = driver.getPageSource();
+        Document document = Jsoup.parse(html);
+
+        Elements productCards = document.select(".d-md-h.ncss-col-sm-12.va-sm-t.pb0-sm.prl0-sm");
+        ArrayList<WebProductData> results = new ArrayList<>();
+        for (Element productCard : productCards) {
+
+            // 제품명 추출
+            String productName = productCard.select(".d-sm-tc .headline-5").text();
+
+            // 출시날짜 추출
+            String[] dates = productCard.select(".d-sm-tc .available-date-component").text().split("[\\.\\s]+");
+            String month = dates[0];
+            String day = dates[1];
+            String ampm = dates[2];
+            String time = dates[3].replace("출시", "");
+
+            // 이미지 URL 추출
+            String imageUrl = productCard.select(".card-link img.image-component").attr("src");
+
+            // 결과 저장
+            results.add(new WebProductData(productName, imageUrl, month, day, ampm, time));
+        }
+        return results;
+    }
+}
---
+++ src/main/java/com/ward/ward_server/webcrawling/WebProductData.java
@@ -0,0 +1,15 @@
+package com.ward.ward_server.webcrawling;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class WebProductData {
+    private String name;
+    private String imgUrl;
+    private String month;
+    private String day;
+    private String ampm;
+    private String time;
+}
