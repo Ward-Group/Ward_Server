80ce5c4079d0e6d586b825afd12f059126f46a3f
s0nnyday <lijnar100@naver.com>
2024-05-27 04:48:40 +0900

    fix: Cors 설정 수정 등 보안 설정 추가
    CorsMvcConfig.java 는 중복 기능이라 제거
    Config 파일에서 한번에 Cors 설정 진행
    허용 주소, 메서드, 헤더 구체화하여 보안 설정
    dto 에 valid 설정 추가 적용해보기

--- src/main/java/com/ward/ward_server/api/user/dto/LoginRequest.java
+++ src/main/java/com/ward/ward_server/api/user/dto/LoginRequest.java
@@ -1,5 +1,7 @@
 package com.ward.ward_server.api.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,8 +12,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
+
+    @NotBlank(message = "Provider cannot be empty")
     private String provider;
+
+    @NotBlank(message = "Provider ID cannot be empty")
     private String providerId;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Email should be valid")
     private String email;
-//    private String password; 가짜 비밀번호로 대체
 }
