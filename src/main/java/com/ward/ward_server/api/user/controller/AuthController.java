e687a73835ccc24ea3aeb4eb1125f987a6b425af
s0nnyday <lijnar100@naver.com>
2024-05-28 15:53:32 +0900

    refactor: AuthController 응답 개선 ApiResponseMessage 추가
    ApiResponse 로 응답할 때 메세지 추가
    ApiResponseMessage 에 필요한 값 추가

--- src/main/java/com/ward/ward_server/api/user/controller/AuthController.java
+++ src/main/java/com/ward/ward_server/api/user/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.ward.ward_server.api.user.dto.LoginRequest;
 import com.ward.ward_server.api.user.dto.RegisterRequest;
 import com.ward.ward_server.api.user.service.AuthService;
 import com.ward.ward_server.global.response.ApiResponse;
+import com.ward.ward_server.global.response.ApiResponseMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,30 +21,30 @@ public class AuthController {
     @PostMapping("/login")
     public ApiResponse<JwtTokens> login(@RequestBody @Validated LoginRequest request) {
         JwtTokens tokens = authService.attemptLogin(request.getProvider(), request.getProviderId(), request.getEmail(), properties.getPassword());
-        return ApiResponse.ok(tokens);
+        return ApiResponse.ok(ApiResponseMessage.LOGIN_SUCCESS, tokens);
     }
 
     @PostMapping("/refresh")
     public ApiResponse<JwtTokens> refresh(@RequestParam("refreshToken") String refreshToken) {
         JwtTokens tokens = authService.refresh(refreshToken);
-        return ApiResponse.ok(tokens);
+        return ApiResponse.ok(ApiResponseMessage.TOKEN_REFRESH_SUCCESS, tokens);
     }
 
     @PostMapping
     public ApiResponse<JwtTokens> register(@RequestBody @Validated RegisterRequest request) {
         JwtTokens tokens = authService.registerUser(request);
-        return ApiResponse.ok(tokens);
+        return ApiResponse.ok(ApiResponseMessage.SIGNUP_SUCCESS, tokens);
     }
 
     @GetMapping("/checkNickname")
     public ApiResponse<Boolean> checkDuplicateNickname(@RequestParam("nickname") String nickname) {
         boolean checkDuplicateNickname = authService.checkDuplicateNickname(nickname);
-        return ApiResponse.ok(checkDuplicateNickname);
+        return ApiResponse.ok(ApiResponseMessage.NICKNAME_CHECK_SUCCESS, checkDuplicateNickname);
     }
 
     @PostMapping("/logout")
     public ApiResponse<Void> logout(@RequestParam("refreshToken") String refreshToken) {
         authService.invalidateRefreshToken(refreshToken);
-        return ApiResponse.ok();
+        return ApiResponse.ok(ApiResponseMessage.LOGOUT_SUCCESS);
     }
 }
--- src/main/java/com/ward/ward_server/global/response/ApiResponseMessage.java
+++ src/main/java/com/ward/ward_server/global/response/ApiResponseMessage.java
@@ -9,6 +9,9 @@ public enum ApiResponseMessage {
     //유저 회원가입 및 로그인
     SIGNUP_SUCCESS("회원가입에 성공하였습니다."),
     LOGIN_SUCCESS("로그인에 성공하였습니다."),
+    TOKEN_REFRESH_SUCCESS("토큰 갱신에 성공하였습니다."),
+    NICKNAME_CHECK_SUCCESS("닉네임 중복 체크에 성공하였습니다."),
+    LOGOUT_SUCCESS("로그아웃에 성공하였습니다."),
     //웹 크롤러
     WEBCRAWLING_NIKE_SUCCESS("나이키 데이터 로드에 성공하였습니다."),
     WEBCRAWLING_KASINA_SUCCESS("카시나 데이터 로드에 성공하였습니다."),
@@ -18,7 +21,7 @@ public enum ApiResponseMessage {
     ITEM_LIST_LOAD_SUCCESS("상품 목록 조회에 성공하였습니다."),
     ITEM_UPDATE_SUCCESS("상품 수정에 성공하였습니다."),
     ITEM_DELETE_SUCCESS("상품 삭제에 성공하였습니다."),
-
+    //브랜드
     BRAND_CREATE_SUCCESS("브랜드 등록에 성공하였습니다."),
     BRAND_TOP10_WITH_ITEM_LOAD_SUCCESS("top10 브랜드와 상품 목록 조회에 성공하였습니다."),
     BRAND_UPDATE_SUCCESS("브랜드 수정에 성공하였습니다."),
@@ -45,5 +48,6 @@ public enum ApiResponseMessage {
     WISH_BRAND_CREATE_SUCCESS("관심브랜드 등록에 성공하였습니다."),
     WISH_BRAND_LOAD_SUCCESS("회원의 관심브랜드 목록 조회에 성공하였습니다."),
     WISH_BRAND_DELETE_SUCCESS("관심브랜드 삭제에 성공하였습니다.");
+
     private final String message;
 }
