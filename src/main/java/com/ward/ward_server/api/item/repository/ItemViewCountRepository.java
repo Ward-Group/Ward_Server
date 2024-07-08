69a23d11f81faefb1a47dfadcc842ca637672426
s0nnyday <lijnar100@naver.com>
2024-07-08 18:00:05 +0900

    refactor: 미사용 코드 제거

--- src/main/java/com/ward/ward_server/api/item/repository/ItemViewCountRepository.java
+++ src/main/java/com/ward/ward_server/api/item/repository/ItemViewCountRepository.java
@@ -1,18 +1,14 @@
 package com.ward.ward_server.api.item.repository;
 
-import com.ward.ward_server.api.item.entity.Item;
 import com.ward.ward_server.api.item.entity.ItemViewCount;
-import com.ward.ward_server.api.item.entity.enums.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface ItemViewCountRepository extends JpaRepository<ItemViewCount, Long> {
-    Optional<ItemViewCount> findByItemAndCategory(Item item, Category category);
 
     @Query("SELECT ivc FROM ItemViewCount ivc " +
             "WHERE ivc.calculatedAt BETWEEN :startTime AND :endTime")
