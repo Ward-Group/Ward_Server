a5c7bd37ced859e0d68f37319eb630bc928a26a3
s0nnyday <lijnar100@naver.com>
2024-07-24 18:15:19 +0900

    feat: 닉네임 조건 추가(회원가입, 닉네임 변경 시)
    1) 필수 입력
    2) 중복된 닉네임 사용 불가
    3) 한글/영문/숫자만 입력 가능
    4) 최대 8자까지 입력 가능

--- src/main/java/com/ward/ward_server/api/user/service/UserService.java
+++ src/main/java/com/ward/ward_server/api/user/service/UserService.java
@@ -4,6 +4,7 @@ import com.ward.ward_server.api.user.entity.User;
 import com.ward.ward_server.api.user.repository.UserRepository;
 import com.ward.ward_server.global.exception.ApiException;
 import com.ward.ward_server.global.exception.ExceptionCode;
+import com.ward.ward_server.global.util.ValidationUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,7 @@ public class UserService {
     public void updateNickname(Long userId, String newNickname) {
         User user = userRepository.findById(userId).orElseThrow(() -> new ApiException(ExceptionCode.USER_NOT_FOUND));
         boolean isNicknameDuplicate = checkDuplicateNickname(newNickname);
+        ValidationUtils.validateNickname(newNickname, isNicknameDuplicate);
         user.changeNickname(newNickname, isNicknameDuplicate);
     }
 
