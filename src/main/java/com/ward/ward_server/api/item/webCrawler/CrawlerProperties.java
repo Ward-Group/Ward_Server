64a8dd239852d99446a6bcf3ae38d24e0530ab1a
s0nnyday <lijnar100@naver.com>
2024-08-17 23:59:38 +0900

    refactor: @Setter 삭제

--- src/main/java/com/ward/ward_server/api/item/webCrawler/CrawlerProperties.java
+++ src/main/java/com/ward/ward_server/api/item/webCrawler/CrawlerProperties.java
@@ -1,12 +1,10 @@
 package com.ward.ward_server.api.item.webCrawler;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
-@Setter
 @Configuration
 @ConfigurationProperties(prefix = "crawler")
 public class CrawlerProperties {
