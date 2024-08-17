5a0e030b8df22c26e12cfc38981742ab38349d07
s0nnyday <lijnar100@naver.com>
2024-08-18 00:25:50 +0900

    refactor: @ConfigurationProperties 사용을 위해 @Setter 예외적 사용
    - @ConfigurationProperties를 사용하여 외부 설정 파일의 값을 자동으로 주입받기 위해 CrawlerProperties 클래스에 @Setter를 추가했습니다.
    - 설정 클래스는 상태 변경에 따른 위험이 적어, 이 경우 @Setter를 사용하는 것으로 하였습니다.
    - 해당 부분에서 @Setter 빼야한다거나 사용 안하는 방법을 공부하면다시 적용하겠습니다.

--- src/main/java/com/ward/ward_server/api/item/webCrawler/CrawlerProperties.java
+++ src/main/java/com/ward/ward_server/api/item/webCrawler/CrawlerProperties.java
@@ -1,11 +1,13 @@
 package com.ward.ward_server.api.item.webCrawler;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Configuration
+@Setter
 @ConfigurationProperties(prefix = "crawler")
 public class CrawlerProperties {
     private String chromeDriverPath;
